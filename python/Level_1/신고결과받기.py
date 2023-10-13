ec95d0726e5772e1dc2ac5e0d231333d6c018968
ghdaud30 <ghdaud3093@gmail.com>
2023-10-14 00:28:18 +0900

    Revert "신고결과받기 (x o)"
    This reverts commit c51b4972357b0801a9c757ef575b5afe2358b436.

--- python/Level_1/신고결과받기.py
+++ python/Level_1/신고결과받기.py
@@ -1,20 +1,17 @@
-id_list = ["a", "ab", "abc", "b"]
-report = ["ab a", "ab abc", "ab b", "abc a", "abc ab", "abc b"]
-k = 2
+id_list = ["con", "ryan"]
+report = ["ryan con", "ryan con", "ryan con", "ryan con"]
+k = 3
 
 def solution(id_list, report, k):
     answer = []
-    report =  list(set(report))
     mine = {i : [] for i in id_list}
     declaration = {i : 0 for i in id_list}
     self_ban = {i : 0 for i in id_list}
     stop_user = []
 
-    #딕셔너리에서 처음 신고된 값만 허용 되게 사용
-    for user in report:     
+    for user in report:
         me,you = user.split(' ')
 
-        #신고한 아이디와 신고당한 횟수 추가
         if(self_ban[me] != you):
             self_ban[me] = you
             mine[me].append(you)
@@ -25,13 +22,11 @@ def solution(id_list, report, k):
     print(mine)
     print(declaration)
     
-    # 신고 횟수가 k번 이상인 아이디를 리스트에 추가
     for key,v in declaration.items():
-        if(v >= k):
+        if(v == k):
             stop_user.append(key)
     print(stop_user)
 
-    # 신고한 아이디를 리스트와 비교하여 answer에 값 추가
     for key,v in mine.items():
         count = 0
         for value in v:
