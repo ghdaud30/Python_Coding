32e4cdf868e66cbd94f0b3ca296681beccc7bb96
ghdaud30 <ghdaud3093@gmail.com>
2023-10-20 23:32:52 +0900

    수박수박수박 , 문자열 다루기 기본

---
+++ python/Level_1/문자열_다루기_기본.py
@@ -0,0 +1,8 @@
+s = '1234'
+
+def solution(s):
+    if((len(s) == 4 or len(s) == 6) and s.isdigit()):
+        return True
+    return False
+
+print(solution(s))
--- python/Level_2/최댓값과_최솟값.py
+++ python/Level_2/최댓값과_최솟값.py
@@ -2,12 +2,8 @@ s = "1 2 3 4"
 
 
 def solution(s):
-    answer = ''
     s = list(map(int ,s.split()))
     print(s)
-    answer += str(min(s))
-    answer += ' '
-    answer += str(max(s))
 
     return str(min(s)) + ' ' + str(max(s))
 
