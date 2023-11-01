c5d8ae5170aab48424d581f646209a1ffea6b80c
ghdaud30 <ghdaud3093@gmail.com>
2023-11-01 19:41:54 +0900

    2023-11-01

--- python/Level_2/전화번호_목록.py
+++ python/Level_2/전화번호_목록.py
@@ -1,5 +1,21 @@
-phone_book = ["119", "97674223", "1195524421"]
+phone_book = ["12","123","1235","567","88"]
 
 def solution(phone_book):
     answer = True
+    # 문자열 리스트를 작은크기부터 정렬해줍니다
+    phone_book.sort()
+    print(phone_book)
+    
+    # 그후 자기 값과 다음 값을 비교하여 
+    # 현재값이 다음값에 접두사인지 확인합니다
+    for i in range(len(phone_book)):
+        try:
+            pre = len(phone_book[i])
+            if(phone_book[i] in phone_book[i+1][:pre]):
+                return False
+        except IndexError:
+            pass
+
     return answer
+
+print(solution(phone_book))
