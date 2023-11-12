da3fdf9683abdc255dbc981c40ddc20eaf785ef6
ghdaud30 <ghdaud3093@gmail.com>
2023-11-12 23:38:14 +0900

    게임 맵 최단 거리 BFS 사용

--- python/Level_2/게임맵최단거리.py
+++ python/Level_2/게임맵최단거리.py
@@ -1,10 +1,6 @@
 from collections import deque
 
-maps = [[1,0,1,1,1],
-        [1,0,1,0,1],
-        [1,0,1,1,1],
-        [1,1,1,0,1],
-        [0,0,0,0,1]]
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]	
 
 def solution(maps):
     answer = 0
@@ -13,29 +9,26 @@ def solution(maps):
     n = len(maps)
     m = len(maps[0])
     moves = [(0,1),(0,-1),(1,0),(-1,0)] 
-    visited = [[False for _ in range(m)] for _ in range(n)]
+    visited = [[False]*m for _ in range(n)]
     queue = deque()
     queue.append((x,y))
+    #처음 좌표를 큐에 넣고 나서 방문 처리를 해줍니다
     visited[x][y] = True
     
     while(queue):
+        # BFS를 이용해서 주변을 돌고 난 후에 pop을 해줍니다
         x,y = queue.popleft()
 
         for move in moves:
-            dx = move[0]
-            dy = move[1]
-            if(x + dx >= n or x + dx < 0 or y + dy >= m or y + dy < 0):
-                continue
-            if(maps[x+dx][y+dy] == 0):
-                continue
-            else:
-                if(maps[x+dx][y+dy] == 1 and visited[x+dx][y+dy] == False):
-                    
-                    maps[x+dx][y+dy] += maps[x][y]
-                    x += dx
-                    y += dy
-                    visited[x][y] = True
-                    queue.append((x,y))
+            nx = x + move[0]
+            ny = y + move[1]
+            if(nx >= n or nx < 0 or ny >= m or ny < 0 or maps[nx][ny] == 0):
+                pass
+            else:     
+                if(not visited[nx][ny]):
+                    maps[nx][ny] += maps[x][y]
+                    visited[nx][ny] = True
+                    queue.append((nx,ny))
     
     print(maps)
     
